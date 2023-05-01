382ad1a3032c1dbb5d99e909fa8769adf29701d3
Sonny Kong <whysonnykong@gmail.com>
2023-05-01 22:25:21 +0100

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base:'/',
-  plugins: [react()],
   build: {
     rollupOptions: {
-      input: '/src/main.tsx'
+      input: 'src/main.tsx',
     },
   },
-})
+});
